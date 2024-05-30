a98d69110a629cd12ae22b3258511299e295b287
liutingli <liutingli@liutinglideair.hitronhub.home>
2024-05-31 00:26:22 +0800

    LoginFilter

--- src/main/java/com/example/demo/filter/LoginFilter.java
+++ src/main/java/com/example/demo/filter/LoginFilter.java
@@ -19,19 +19,19 @@ import jakarta.servlet.http.HttpSession;
 
 @WebFilter("/form_auth/*")
 public class LoginFilter implements Filter {
-	
+
 	static Map<String, Map<String, String>> users = new HashMap<>();
-	static{
-		users.put("user", Map.of("hash", "7723786a93d89c62a3f875d92daf585550fe3669ff4b997d7730bb39d53d20c9", 
-				"salt", "ab6ee905e4f60232c73fb8323270948f")); // 1234
-		users.put("John", Map.of("hash", "e19a311a6ff13fe3c3dad73a37f3243eb8be7c2e9d15df77df2ef8095ff41e0f", 
-				"salt", "6768fa0bffa5fa914736932e2e293014")); // 1234
-		users.put("Mary", Map.of("hash", "33cddbb9d7386658408cbe23c1f01a6c5d7398e019da1658228db819d71d7e93", 
-				"salt", "2591b7b69f6bd8e7d375eaaa3d98d8b9")); // 5678
-		users.put("Irin", Map.of("hash", "9c8b6cf19894a5d97098bde9fa71808ba0c6c6ccad4610f181b1cfe827a6eccf", 
-				"salt", "9cccf63bad04dbeb1764499dbd4ced9f"));// 1234
-		users.put("Nikolas", Map.of("hash", "5ce6cfcef667e8e546bb04a19d67bcd5a70522a587551040450733850bf10fa4", 
-				"salt", "86c1d795db43ecc7cfd999c8530498f4"));// 1234
+	static {
+		users.put("user", Map.of("hash", "7723786a93d89c62a3f875d92daf585550fe3669ff4b997d7730bb39d53d20c9", "salt",
+				"ab6ee905e4f60232c73fb8323270948f")); // 1234
+		users.put("John", Map.of("hash", "e19a311a6ff13fe3c3dad73a37f3243eb8be7c2e9d15df77df2ef8095ff41e0f", "salt",
+				"6768fa0bffa5fa914736932e2e293014")); // 1234
+		users.put("Mary", Map.of("hash", "33cddbb9d7386658408cbe23c1f01a6c5d7398e019da1658228db819d71d7e93", "salt",
+				"2591b7b69f6bd8e7d375eaaa3d98d8b9")); // 5678
+		users.put("Irin", Map.of("hash", "9c8b6cf19894a5d97098bde9fa71808ba0c6c6ccad4610f181b1cfe827a6eccf", "salt",
+				"9cccf63bad04dbeb1764499dbd4ced9f"));// 1234
+		users.put("Nikolas", Map.of("hash", "5ce6cfcef667e8e546bb04a19d67bcd5a70522a587551040450733850bf10fa4", "salt",
+				"86c1d795db43ecc7cfd999c8530498f4"));// 1234
 	}
 
 	@Override
@@ -44,37 +44,47 @@ public class LoginFilter implements Filter {
 
 		// 判斷使用者是否已登入
 		Object loginStatus = session.getAttribute("loginStatus");
-		if(loginStatus != null && Boolean.valueOf(loginStatus+"")) {
+		if (loginStatus != null && Boolean.valueOf(loginStatus + "")) {
 			// 放行
 			chain.doFilter(request, response);
-			
+
 		} else {
 			// 驗證 username 和 password
 			String username = req.getParameter("username");
 			String password = req.getParameter("password");
-			
+
 			// 如果沒有 username 的資料代表是第一次登入，重導至登入頁面
-			if(username == null || username.trim().length() == 0) {
+			if (username == null || username.trim().length() == 0) {
 				resp.sendRedirect("/login");
 				return;
-			} 
+			}
 			
+			// 判斷 csrfToken 令牌是否有效 ?
+			String csrfToken = req.getParameter("csrfToken"); // 表單傳來的 token
+			String csrfTokenFromSession = session.getAttribute("csrfToken") + "";
+
+			if (!csrfTokenFromSession.equals(csrfToken)) {
+				System.out.println("CSRF Token 錯誤");
+				resp.sendRedirect("/login");
+				return;
+			}
+
 			// 是否有此 user
 			Map<String, String> user = users.get(username);
-			if(user==null) {
+			if (user == null) {
 				System.out.println("無此使用者");
 				resp.sendRedirect("/login");
 				return;
-			} 
-			
+			}
+
 			// 判斷 password
 			// 得到使用者的 hash 與 salt
 			String hash = user.get("hash");
 			String saltString = user.get("salt");
 			byte[] salt = WebKeyUtil.hexStringToByteArray(saltString);
-			
+
 			// 透過密碼跟鹽巴算出來的 hash 要跟資料庫中的 hash 比對
-			
+
 			try {
 				MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
 				messageDigest.reset(); // 重置
@@ -83,7 +93,7 @@ public class LoginFilter implements Filter {
 				// 根據使用者輸入的 password 與已知的 salt 來產出 comparedHash
 				String comparedHash = WebKeyUtil.bytesToHexString(inputHashedBytes);
 				// 比較 comparedHash(運算的) 與 hash(已儲存的) 是否相等
-				if(comparedHash.equals(hash)) {
+				if (comparedHash.equals(hash)) {
 					// 儲存登入狀態
 					session.setAttribute("loginStatus", true);
 					// 放行
@@ -94,14 +104,14 @@ public class LoginFilter implements Filter {
 					resp.sendRedirect("/login");
 					return;
 				}
-				
+
 			} catch (Exception e) {
 				e.printStackTrace();
 				resp.sendRedirect("/login");
 			}
-			
+
 		}
-		
+
 	}
 
 }
